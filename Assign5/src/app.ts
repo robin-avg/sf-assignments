851e4ee3aabb2196367841f6c14745ee581b92a2
Robin Singh <robin.singh@sourcefuse.com>
2021-07-20 22:55:00 +0530

    crudApis

--- Assign5/src/app.ts
+++ Assign5/src/app.ts
@@ -1,31 +1,96 @@
 import express, { Application, Request, Response } from 'express';
 import path from 'path';
 import fs from 'fs';
-
+import bodyParser from 'body-parser';
+import cors from 'cors';
 
 const app: Application = express();
 
-//app.set('views', path.join(__dirname, "views"))
-app.set('view engine', 'ejs');
-app.use(express.static(path.join(__dirname, "../logic")));
+app.use(express.json());
 
-var jsonData: any;
-app.get('/', (req: Request, res: Response) => {
-    res.render('index');
+//READ
+app.get('/user/list', (req: Request, res: Response) => {
+    const users = getUserData();
+    //  console.log(users);
+    res.send(users);
 })
 
-app.get('/read', (req: Request, res: Response) => {
-    fs.readFile('users.json', 'utf-8', (err, content) => {
-        if (err) {
-            console.log(err);
-        } else {
-            jsonData = JSON.parse(content);
-            console.log(jsonData);
-        }
-    })
-    res.render('read', { jsonData: jsonData });
+//CREATE
+app.post('/user/add', (req, res) => {
+    //get the existing user data
+    const existUsers = getUserData();
+
+    //get the new user data from post request
+    const userData = req.body
+    //check if the userData fields are missing
+    if (userData.name == null || userData.contact == null || userData.email == null || userData.roll == null || userData.role == null) {
+        return res.status(401).send({ error: true, msg: 'User data missing' })
+    }
+
+    //check if the username exist already
+    const findExist = existUsers.find((user: any) => user.name === userData.name)
+    if (findExist) {
+        return res.status(409).send({ error: true, msg: 'username already exist' })
+    }
+    //append the user data
+    existUsers.push(userData)
+    //save the new user data
+    saveUserData(existUsers);
+    res.send({ success: true, msg: 'User data added successfully' })
 })
 
+//UPDATE
+
+app.patch('/user/update/:username', (req, res) => {
+    //get the username from url
+    const username = req.params.username
+    //get the update data
+    const userData = req.body
+    //get the existing user data
+    const existUsers = getUserData()
+    //check if the username exist or not       
+    const findExist = existUsers.find((user: any) => user.name === username)
+    if (!findExist) {
+        return res.status(409).send({ error: true, msg: 'username not exist' })
+    }
+    //filter the userdata
+    const updateUser = existUsers.filter((user: any) => user.name !== username)
+    //push the updated data
+    updateUser.push(userData)
+    //finally save it
+    saveUserData(updateUser)
+    res.send({ success: true, msg: 'User data updated successfully' })
+})
+
+
+//DELETE
+app.delete('/user/delete/:username', (req: Request, res: Response) => {
+    const username = req.params.username
+    //get the existing userdata
+    const existUsers = getUserData()
+    //filter the userdata to remove it
+    const filterUser = existUsers.filter((user: any) => user.name !== username)
+    if (existUsers.length === filterUser.length) {
+        return res.status(409).send({ error: true, msg: 'username does not exist' })
+    }
+    //save the filtered data
+    saveUserData(filterUser)
+    res.send({ success: true, msg: 'User removed successfully' })
+
+})
+
+/*UTIL FUNCTION*/
+//get the user data from json file
+const getUserData = () => {
+    const jsonData: any = fs.readFileSync('users.json')
+    return JSON.parse(jsonData)
+}
+
+//read the user data from json file
+const saveUserData = (data: any) => {
+    const stringifyData = JSON.stringify(data)
+    fs.writeFileSync('users.json', stringifyData)
+}
 
 
 app.listen(5000, () => console.log('server running'));
